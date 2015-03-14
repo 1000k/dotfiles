d5c6ba951d52e7ecba1ecb4b8cdfd58ec0b4f241
SENDA <senda.keijiro@gmail.com>
2015-03-14 20:24:08 +0900

    Improve windows setup shell to install NeoBundle

---
+++ .gvimrc
@@ -0,0 +1,3 @@
+" Enable backspace
+set backspace=2
+set backspace=indent,eol,start
--- setup.cmd
+++ setup.cmd
@@ -6,3 +6,7 @@ set dotfiles_dir="%USERPROFILE%\dotfiles"
 mklink "%home_dir%\_vimrc" "%dotfiles_dir%\.vimrc" 
 mklink "%home_dir%\_gvimrc" "%dotfiles_dir%\.gvimrc"
 
+md "%home_dir%\.vim"
+md "%home_dir%\.vim\bundle"
+git clone https://github.com/Shougo/neobundle.vim "%home_dir%\.vim\bundle\neobundle.vim"
+
