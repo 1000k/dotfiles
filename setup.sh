ab016ae258f810c8fb48ea2d752184f4e70bed71
SENDA Keijiro <senda.keijiro@gmail.com>
2015-03-16 14:22:34 +0900

    Implement setup.sh to make symlink

--- setup.sh
+++ setup.sh
@@ -4,3 +4,11 @@ files=(.vimrc .zshrc)
 for item in ${files[@]}; do
   ln -sf ~/dotfiles/${item} ~/${item}
 done
+
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+
+ln -sf ~/dotfiles/colors ~/.vim/colors
+ln -sf ~/dotfiles/indent ~/.vim/indent
+
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
