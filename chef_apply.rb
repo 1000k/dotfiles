84ad26e3f0b87630166855ea9bbf554d3439a609
SENDA Keijiro <senda.keijiro@gmail.com>
2015-03-20 14:52:25 +0900

    Add install recipe for tmux 1.9a

--- chef_apply.rb
+++ chef_apply.rb
@@ -1,7 +1,12 @@
+# Usage: `sudo chef-apply chef_apply.rb`
+
+cache_dir = Chef::Config['file_cache_path']
+
 # ----
 # install vim
 # ----
 vim_version = '7.4'
+
 %w{
   ruby-devel
   lua
@@ -20,8 +25,8 @@ vim_version = '7.4'
   package pkg
 end
 
-execute 'install-vim-from-source' do
-  cwd '/tmp/'
+execute 'install_vim_from_source' do
+  cwd cache_dir
   command <<-CMD
     hg clone https://code.google.com/p/vim/
     cd vim
@@ -37,12 +42,51 @@ execute 'install-vim-from-source' do
     make VIMRUNTIMEDIR=/usr/share/vim/vim74
     make install
   CMD
-  not_if { "which vim | grep #{vim_version}" }
+  not_if "which vim | grep #{vim_version}"
 end
 
 
 # ----
 # install tmux
 # ----
+tmux_version = '1.9a'
 
+%w{wget gcc make ncurses ncurses-devel}.each { |pkg| package pkg }
+
+# tmux 1.9 needs recent libevent (not yum version)
+libevent_version = '2.0.22-stable'
+
+remote_file "#{cache_dir}/libevent-#{libevent_version}.tar.gz" do
+  source 'https://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-2.0.22-stable.tar.gz'
+  mode '0644'
+  not_if { File.exists?("#{cache_dir}/libevent-#{libevent_version}.tar.gz") }
+end
+
+execute "install_libevent-#{libevent_version}" do
+  cwd cache_dir
+  command <<-CMD
+    tar zxvf libevent-#{libevent_version}.tar.gz
+    cd libevent-#{libevent_version}
+    ./configure && make && make install
+    echo '/usr/local/lib' > /etc/ld.so.conf.d/libevent.conf
+    ldconfig
+  CMD
+  not_if { File.exists? '/etc/ld.so.conf.d/libevent.conf' }
+end
+
+remote_file "#{cache_dir}/tmux-#{tmux_version}.tar.gz" do
+  source "http://downloads.sourceforge.net/tmux/tmux-#{tmux_version}.tar.gz"
+  mode '0644'
+  not_if { File.exists?("#{cache_dir}/tmux-#{tmux_version}.tar.gz") }
+end
+
+execute "install_tmux-#{tmux_version}" do
+  cwd cache_dir
+  command <<-CMD
+    tar zxvf tmux-#{tmux_version}.tar.gz
+    cd tmux-#{tmux_version}
+    ./configure && make && make install
+  CMD
+  not_if "tmux -V | grep #{tmux_version}"
+end
 
