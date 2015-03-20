5664f71ddf1c07be3eaa7446ff6ef17781f19e8f
SENDA Keijiro <senda.keijiro@gmail.com>
2015-03-20 11:36:23 +0900

    Add tmux installation commands

--- setup.sh
+++ setup.sh
@@ -5,10 +5,14 @@ for item in ${files[@]}; do
   ln -sf ~/dotfiles/${item} ~/${item}
 done
 
+# Vim
 mkdir ~/.vim
 mkdir ~/.vim/bundle
-
 ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/indent ~/.vim/indent
-
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# tmux
+mkdir ~/.tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
