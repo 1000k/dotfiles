7a620a658a5e60bc47a4d7a3938a4e41624db1b0
SENDA Keijiro <senda.keijiro@gmail.com>
2015-06-02 17:06:23 +0900

    Enable installing vim from source

--- chef_apply.rb
+++ chef_apply.rb
@@ -25,25 +25,25 @@ vim_version = '7.4'
   package pkg
 end
 
-#execute 'install_vim_from_source' do
-#  cwd cache_dir
-#  command <<-CMD
-#    hg clone https://code.google.com/p/vim/
-#    cd vim
-#    ./configure --with-features=huge \
-#      --enable-multibyte \
-#      --enable-rubyinterp \
-#      --enable-pythoninterp \
-#      --with-python-config-dir=/usr/lib/python2.7/config \
-#      --enable-perlinterp \
-#      --enable-luainterp \
-#      --enable-cscope \
-#      --prefix=/usr
-#    make VIMRUNTIMEDIR=/usr/share/vim/vim74
-#    make install
-#  CMD
-#  not_if "which vim | grep #{vim_version}"
-#end
+execute 'install_vim_from_source' do
+  cwd cache_dir
+  command <<-CMD
+    hg clone https://code.google.com/p/vim/
+    cd vim
+    ./configure --with-features=huge \
+      --enable-multibyte \
+      --enable-rubyinterp \
+      --enable-pythoninterp \
+      --with-python-config-dir=/usr/lib/python2.7/config \
+      --enable-perlinterp \
+      --enable-luainterp \
+      --enable-cscope \
+      --prefix=/usr
+    make VIMRUNTIMEDIR=/usr/share/vim/vim74
+    make install
+  CMD
+  not_if "which vim | grep #{vim_version}"
+end
 
 
 # ----
