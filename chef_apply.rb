3e358955fea53bd421f268bdfe62ee93913565fc
SENDA Keijiro <senda.keijiro@gmail.com>
2017-04-12 20:04:31 +0900

    Remove CentOS recipes and add Ubuntu

--- chef_apply.rb
+++ chef_apply.rb
@@ -5,93 +5,15 @@ cache_dir = Chef::Config['file_cache_path']
 %w{zsh ctags}.each {|pkg| package pkg}
 Chef::Log.info 'After install zsh, enter `chsh -s /bin/zsh` and restart terminal.'
 
-# ----
-# install vim
-# ----
-vim_version = '7.4'
+case node['platform']
+when 'ubuntu'
+  # install Vim 8
+  apt_repository 'add-vim-repo' do
+    uri 'ppa:jonathonf/vim'
+    distribution node['lsb']['codename']
+  end
 
-%w{
-  ruby-devel
-  lua
-  lua-devel
-  ctags
-  mercurial
-  python
-  python-devel
-  tcl-devel
-  perl
-  perl-devel
-  perl-ExtUtils-ParseXS
-  perl-ExtUtils-CBuilder
-  perl-ExtUtils-Embed
-}.each do |pkg|
-  package pkg
+  package 'vim' do
+    action :upgrade
+  end
 end
-
-execute 'install_vim_from_source' do
-  cwd cache_dir
-  command <<-CMD
-    hg clone https://code.google.com/p/vim/
-    cd vim
-    ./configure --with-features=huge \
-      --enable-multibyte \
-      --enable-rubyinterp \
-      --enable-pythoninterp \
-      --with-python-config-dir=/usr/lib/python2.7/config \
-      --enable-perlinterp \
-      --enable-luainterp \
-      --enable-cscope \
-      --prefix=/usr
-    make VIMRUNTIMEDIR=/usr/share/vim/vim74
-    make install
-  CMD
-  not_if "which vim | grep #{vim_version}"
-end
-
-
-# ----
-# install tmux
-# ----
-package 'xsel' # dependency for tmux-yank
-
-tmux_version = '1.9a'
-
-%w{wget gcc make ncurses ncurses-devel}.each { |pkg| package pkg }
-
-# tmux 1.9 needs recent libevent (not yum version)
-libevent_version = '2.0.22-stable'
-
-remote_file "#{cache_dir}/libevent-#{libevent_version}.tar.gz" do
-  source 'https://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-2.0.22-stable.tar.gz'
-  mode '0644'
-  not_if { File.exists?("#{cache_dir}/libevent-#{libevent_version}.tar.gz") }
-end
-
-execute "install_libevent-#{libevent_version}" do
-  cwd cache_dir
-  command <<-CMD
-    tar zxvf libevent-#{libevent_version}.tar.gz
-    cd libevent-#{libevent_version}
-    ./configure && make && make install
-    echo '/usr/local/lib' > /etc/ld.so.conf.d/libevent.conf
-    ldconfig
-  CMD
-  not_if { File.exists? '/etc/ld.so.conf.d/libevent.conf' }
-end
-
-remote_file "#{cache_dir}/tmux-#{tmux_version}.tar.gz" do
-  source "http://downloads.sourceforge.net/tmux/tmux-#{tmux_version}.tar.gz"
-  mode '0644'
-  not_if { File.exists?("#{cache_dir}/tmux-#{tmux_version}.tar.gz") }
-end
-
-execute "install_tmux-#{tmux_version}" do
-  cwd cache_dir
-  command <<-CMD
-    tar zxvf tmux-#{tmux_version}.tar.gz
-    cd tmux-#{tmux_version}
-    ./configure && make && make install
-  CMD
-  not_if "tmux -V | grep #{tmux_version}"
-end
-
