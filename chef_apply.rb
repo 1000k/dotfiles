458e8c16092e15cfb9bab27c8f24ad21095c82c7
SENDA Keijiro <senda.keijiro@gmail.com>
2017-04-20 14:18:30 +0900

    Add installation command to chef_apply.rb

--- chef_apply.rb
+++ chef_apply.rb
@@ -4,16 +4,24 @@ cache_dir = Chef::Config['file_cache_path']
 
 case node['platform']
 when 'ubuntu'
-  %w{zsh ctags}.each {|pkg| package pkg}
-  Chef::Log.info 'After install zsh, enter `chsh -s /bin/zsh` and restart terminal.'
+  codename = node['lsb']['codename']
+  #%w{zsh ctags}.each {|pkg| package pkg}
+  #Chef::Log.info 'After install zsh, enter `chsh -s /bin/zsh` and restart terminal.'
 
   # install Vim 8
-  apt_repository 'add-vim-repo' do
+  apt_repository 'vim' do
     uri 'ppa:jonathonf/vim'
-    distribution node['lsb']['codename']
+    distribution codename
   end
 
   package 'vim' do
     action :upgrade
   end
+
+  apt_repository 'fish' do
+    uri 'ppa:fish-shell/release-2'
+    distribution codename
+  end
+
+  package 'fish'
 end
