e2120bd56e29be9e8eaaad8fa136388607926cf4
SENDA Keijiro <senda.keijiro@gmail.com>
2017-04-28 17:20:11 +0900

    Add peco shortcut

--- config.fish
+++ config.fish
@@ -3,6 +3,7 @@ eval (direnv hook fish)
 set TERM screen-256color
 set fish_plugins theme peco
 
+# Bind for prco history to Ctrl+r
 function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
+  bind \cr peco_select_history
 end
