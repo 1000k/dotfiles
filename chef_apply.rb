12abdd7f99b15919b6ae5cc358c21da8cb2db5fb
SENDA Keijiro <senda.keijiro@gmail.com>
2017-05-10 16:36:16 +0900

    Exclude oh-my-fish

--- chef_apply.rb
+++ chef_apply.rb
@@ -39,15 +39,8 @@ when 'ubuntu'
     distribution codename
   end
 
-  package 'fish' do
-    notifies :run, 'execute[install_fish]'
-  end
+  package 'fish'
   Chef::Log.info 'After install fish, enter `chsh -s /usr/bin/fish` and restart terminal.'
-
-  execute 'install_fish' do
-    command 'curl -L https://get.oh-my.fish | fish'
-    action :nothing
-  end
 
   # Direnv
   remote_file '/usr/local/bin/direnv' do
